27c8861d32d153af692be92a2b4243ce53144c10
Luis-Salas55 <eevooisalas@micorreo.upp.edu.mx>
2026-03-12 13:52:40 -0600

    Actualización de Emergencia
    v0.1.2a

--- Proyecto/frontend/src/app/app.ts
+++ Proyecto/frontend/src/app/app.ts
@@ -2,13 +2,14 @@ import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { Header } from "./components/header/header";
 import { Footer } from "./components/footer/footer";
+import { Navbar } from "./components/navbar/navbar";
 import { NavbarGuest } from "./components/navbar-guest/navbar-guest";
 import { NavbarUser } from "./components/navbar-user/navbar-user";
 import { NavbarAdmin } from "./components/navbar-admin/navbar-admin";
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, Header, NavbarGuest, NavbarAdmin, NavbarUser, Footer],
+  imports: [RouterOutlet, Header, Navbar, NavbarGuest, NavbarAdmin, NavbarUser, Footer],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
