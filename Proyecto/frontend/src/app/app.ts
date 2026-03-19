450d0815f92360b189c705063f0bfee931f4d638
Luis-Salas55 <eevooisalas@micorreo.upp.edu.mx>
2026-03-19 14:25:14 -0600

    Actualización de comportamiento y navbar
    Version 0.3a

--- Proyecto/frontend/src/app/app.ts
+++ Proyecto/frontend/src/app/app.ts
@@ -2,22 +2,12 @@ import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { Header } from "./components/header/header";
 import { Footer } from "./components/footer/footer";
-<<<<<<< Updated upstream
-import { NavbarGuest } from "./components/navbar-guest/navbar-guest";
-import { NavbarUser } from "./components/navbar-user/navbar-user";
-import { NavbarAdmin } from "./components/navbar-admin/navbar-admin";
-
-@Component({
-  selector: 'app-root',
-  imports: [RouterOutlet, Header, NavbarGuest, NavbarAdmin, NavbarUser, Footer],
-=======
 import { Navbar } from "./components/navbar/navbar";
 import { Profile } from "./pages/logged/profile/profile";
 
 @Component({
   selector: 'app-root',
   imports: [RouterOutlet, Header, Navbar, Footer, Profile],
->>>>>>> Stashed changes
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
